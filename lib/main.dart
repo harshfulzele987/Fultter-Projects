bfbea83287bd00b02aa649d5f51260331c39a4a3
harsh fulzel <harshfulzele987@gmail.com>
2021-03-02 16:53:41 +0530

    drawer added in main.dart files

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,11 @@ void main() {
   runApp(MaterialApp(
     title: "Awsome app",
     home: HomePage(),
-  ));
+    theme: ThemeData(
+        primarySwatch: Colors.pink,
+          )
+       )
+  );
 }
 
 class HomePage extends StatelessWidget {
@@ -14,37 +18,59 @@ class HomePage extends StatelessWidget {
         appBar: AppBar(
           title: Text("awsome app"),
         ),
-        body: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly ,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
+        body: Center(
+         
+          child: Card(
+         
+            child: Column(
+         
+           children : <Widget>[ Image.asset("bg.jpg")],
+            ),
+          ),
+        ),
+          drawer: Drawer(
+            child : ListView(
+              children: <Widget>[
+                UserAccountsDrawerHeader(
+                  
+                  accountName: Text("harsh fulzele"),
+                  accountEmail: Text("harsh123@gmail.com"),
+                  currentAccountPicture: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                      "https://www.google.com/search?q=nancy+images&sxsrf=ALeKk01H4pmpeKz6IJ7jaWZ5d2Ra3IcYxA:1614678062200&tbm=isch&source=iu&ictx=1&fir=5Zw-vGvOVC3lwM%252CPUodsIwntC8Z8M%252C_&vet=1&usg=AI4_-kQbG_hSi5qUyyopjdceVL3JrjibDA&sa=X&ved=2ahUKEwiCn8vCqJHvAhWTF3IKHfe3A3sQ9QF6BAgVEAE#imgrc=5Zw-vGvOVC3lwM"
+
+
+                    ),
+                  ),  
+                  ),
+                    
+                    
+                    ListTile(
+                    leading: Icon(Icons.email),
+                    title: Text("Account"), 
+                    subtitle: Text("personal"),
+                    trailing: Icon(Icons.edit),
+                    ),
+             
+               ListTile(
+                    leading: Icon(Icons.email),
+                    title: Text("Account"), 
+                    subtitle: Text("harsh123@gmail.com"),
+                    trailing: Icon(Icons.send),
+                    ),
+             
+              ]
+
+
+
               
               
-              Container(
-              padding: const EdgeInsets.all(8),
-              alignment: Alignment.center,
-              width: 50,
-              height: 50,
-              color: Colors.green,
               ),
-              Container(
-              padding: const EdgeInsets.all(8),
-              alignment: Alignment.center,
-              width: 50,
-              height: 50,
-              color: Colors.red,
-              ),
-              Container(
-              padding: const EdgeInsets.all(8),
-              alignment: Alignment.center,
-              width: 50,
-              height: 50,
-              color: Colors.yellow,
-              ),
-            ]
-              
             
-          
-        ));
-  }
+            
+            )
+     
+     
+      );
+    }
 }
